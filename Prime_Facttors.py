1e53bd6832a19ef44ceea3714ba7e286c744bfd5
Kosmas Nick <n.kosmas89@gmail.com>
2024-11-02 23:45:44 +0200

    Update Prime_Facttors.py

--- Prime_Facttors.py
+++ Prime_Facttors.py
@@ -1,24 +1,22 @@
-#ΥΠΟΕΡΓΑΣΙΑ 3 ΓΙΝΟΜΕΝΟ ΠΡΩΤΩΝ ΠΑΡΑΓΟΝΤΩΝ
-
-print("Γινόμενο Πρώτων Παραγόντων")
+# Prime Factorization
+print("Prime Factorization")
                                                       
-apothikevmenoi_arithmoi = [ ]
+stored_numbers = [ ]
 diairetis = 2            
 
 while True:
     number = int(input("Δώστε το n (2-10000): "))
     if 2 <= number <= 10000:
-        arxikos_arithmos = number                           # βάζουμε τον αριθμό (input) σε μεταβλητή για να τον καλέσουμε στην τελική εκτύπωση
-        break
+        arxikos_arithmos = number                            # We store the number (input) in a variable so that we can call it in the final output.
            
 while number >=2 :
-    if number % diairetis == 0:                               # Αν διαιρείται ακριβώς με τον τρέχοντα διαιρέτη
-        apothikevmenoi_arithmoi.append(diairetis)  # Προσθέτουμε τον διαιρέτη στη λίστα (αποθηκευμένοι αριθμοί)
-        number = number // diairetis                        # Διαιρούμε τον αριθμό με τον διαιρέτη
+    if number % diairetis == 0:                              # If it is divisible exactly by the current divisor.
+       stored_numbers.append(diairetis)            # We add the divisor to the list (stored numbers)
+        number = number // diairetis                         # We divide the number by the divisor
     else:
         diairetis = diairetis + 1  
 
-print(f"{arxikos_arithmos} = {'  x  '.join(map(str, apothikevmenoi_arithmoi))}")
+print(f"{arxikos_arithmos} = {'  x  '.join(map(str, stored_numbers))}")
     
 
      
